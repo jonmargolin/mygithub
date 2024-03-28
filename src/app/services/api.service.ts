6ad3c4e521cfe6dc063a742de419c27173cc4cdc
admin <admin@admins-MBP.lan>
2024-03-28 16:14:07 +0200

    add action

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,9 +8,7 @@ import { taskMock } from '../board-store/task.mock';
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl =
-    'https://frontend-kanban-node-be.azurewebsites.net/api/GetTasks?code=-9J4AUVTzvP7DBtjNPzNDMz_Sc0fK068G-6Ji8GQjOhjAzFuqchHLw==';
-
+ 
   constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
